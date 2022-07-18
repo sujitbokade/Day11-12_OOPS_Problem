f0cca7d4b14040c25cabd60a638df5ef501ab3dd
sujit <sujitbokade2014@gmail.com>
2022-07-18 23:45:59 +0530

    Initial Commit

---
+++ src/com/bridgelabz/account/Account.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.account;
+
+import java.util.Scanner;
+
+public class Account {
+    int debit;
+
+    void debit(int accountBalance){
+
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Enter the amount to debit from account balance");
+        debit = scr.nextInt();
+        if (debit>accountBalance){
+            System.out.println("Debit amount exceeded account balance");
+            System.out.println("Account balance is: "+accountBalance);
+
+        }
+        else{
+            accountBalance-=debit;
+            System.out.println("Remaining account balance is: "+accountBalance);
+        }
+
+    }
+
+    public Account() {
+    }
+}
---
+++ src/com/bridgelabz/account/AccountTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.account;
+
+
+    import java.util.Scanner;
+
+    public class AccountTest extends Account{
+        public static void main(String[] args) {
+            AccountTest test = new AccountTest();
+            Scanner scr = new Scanner(System.in);
+            System.out.println("Enter the account balance");
+            int accountBalance= scr.nextInt();
+            test.debit(accountBalance);
+        }
+    }
+
