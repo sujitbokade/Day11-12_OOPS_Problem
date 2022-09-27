5fd3e731c8fe702f8ef3da9e09db12e556f20e64
sujit <sujitbokade2014@gmail.com>
2022-09-27 09:37:14 +0530

    [Sujit]Modified Program

--- src/com/bridgelabz/account/Account.java
+++ src/com/bridgelabz/account/Account.java
@@ -5,19 +5,18 @@ import java.util.Scanner;
 public class Account {
     int debit;
 
-    void debit(int accountBalance){
+    void debit(int accountBalance) {
 
         Scanner scr = new Scanner(System.in);
         System.out.println("Enter the amount to debit from account balance");
         debit = scr.nextInt();
-        if (debit>accountBalance){
+        if (debit > accountBalance) {
             System.out.println("Debit amount exceeded account balance");
-            System.out.println("Account balance is: "+accountBalance);
+            System.out.println("Account balance is: " + accountBalance);
 
-        }
-        else{
-            accountBalance-=debit;
-            System.out.println("Remaining account balance is: "+accountBalance);
+        } else {
+            accountBalance -= debit;
+            System.out.println("Remaining account balance is: " + accountBalance);
         }
 
     }
